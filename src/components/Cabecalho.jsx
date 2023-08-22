d78791c66693956b03c8551157e4263e92aae663
Rodrigo Lima <rodrigo.blima17@gmail.com>
2023-08-22 11:56:55 -0300

    Update

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function cabecalho() {
+  return (
+    <>
+      <header>
+        <h1>
+          Vite + React
+        </h1>
+    </header>
+    </>
+    )
+}
