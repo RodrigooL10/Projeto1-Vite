35c3e5d21c11f7ba137b545663e55872367bac99
Rodrigo Lima <rodrigo.blima17@gmail.com>
2023-09-05 11:17:45 -0300

    Update 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//BLOCO DE ROTAS
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './routes/Home.jsx';
+import Produtos from './routes/Produtos.jsx';
+import EditarProdutos from './routes/EditarProdutos.jsx';
+import Erro404 from './routes/Erro404.jsx';
+
+const router = createBrowserRouter([
+  {path: "/", element:<App/>, 
+    children:[
+      {path: "/", element:<Home/>},
+      {path: "/", element:<Produtos/>}, 
+      {path: "/", element:<EditarProdutos/>}, 
+      {path: "/", element:<Erro404/>},  
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
